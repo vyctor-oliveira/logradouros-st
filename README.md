001bc5cc9f3e2f0743a4cd7553feeb6845b7dd8e
vyctor-oliveira <vyctor.oliveira.soares@gmail.com>
2026-08-13 00:07:05 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Logradouros de Serra Talhada
+
+Uma maneira mais rápida de visualizar todos as ruas, avenidas, travessas e semelhantes da cidade de Serra Talhada, Pernambuco.
+
+[Dados originais](https://www.google.com/maps/d/u/0/viewer?mid=1c2ju_HcCjBugKl4mwtXQ9lET2bHofd1O&ll=-8.002444472319501%2C-38.290630626708996&z=13) pela Secretaria de Finanças da Prefeitura Municipal de Serra Talhada.
+
+## Como executar?
+
+Com o [R instalado](https://cran.r-project.org/doc/contrib/Itano-installation.pdf) no sistema, basta:
+
+```bash
+Rscript -e getkml.R 
+```
+
+Alternativamente, se você usa [uvr](https://github.com/nbafrank/uvr/):
+
+```bash
+uvr run getkml.R
+```
+
+## Visualização
+
+[Versão para consulta](https://lite.datasette.io)
+
---
+++ getkml.R
@@ -0,0 +1,67 @@
+# /// script
+# dependencies = [
+#    "dplyr",
+#    "tidyr",
+#    "stringr",
+#    "purrr",
+#    "freshr",
+#    "sf",
+#    "janitor"
+# ]
+# ///
+
+freshr::freshr()
+
+url <- "https://www.google.com/maps/d/u/0/kml?mid=1c2ju_HcCjBugKl4mwtXQ9lET2bHofd1O&resourcekey&lid=XTCOnJk9-Is&forcekml=1"
+
+logradouros <- tryCatch(
+  sf::st_read(url, quiet = TRUE),
+  error = function(e) {
+    stop("Falha ao baixar o KML do Google Maps: ", conditionMessage(e))
+  }
+)
+
+# readr::write_rds(logradouros, "logradouros.rds")
+
+logradouros_antigo <- readr::read_rds("logradouros.rds")
+
+diff <- dplyr::setdiff(logradouros, logradouros_antigo)
+
+if (nrow(diff) == 0) {
+  message("Sem novos logradouros. Encerrando.")
+  quit(status = 0)
+}
+
+writeLines(
+  paste(nrow(diff), "novos logradouros"),
+  "message.txt",
+  useBytes = TRUE
+)
+
+logradouros_limpo <- logradouros |>
+  dplyr::mutate(
+    Description = stringr::str_replace(Description, "<br><br>", "<br>")
+  ) |>
+  tidyr::separate(
+    Description,
+    into = c("descricao", "codigo", "bairro", "cep"),
+    sep = "<br>",
+    extra = "merge",
+    fill = "right"
+  ) |>
+  dplyr::mutate(
+    dplyr::across(
+      c(descricao, codigo, bairro, cep),
+      \(x) stringr::str_remove(x, ".*: ")
+    )
+  ) |>
+  janitor::clean_names()
+
+purrr::walk(
+  list(
+    c("logradouros.geojson", "GeoJSON"),
+    c("logradouros.parquet", "Parquet"),
+    c("logradouros.csv", "CSV")
+  ),
+  ~ sf::write_sf(logradouros_limpo, .x[1], driver = .x[2], quiet = TRUE)
+)
